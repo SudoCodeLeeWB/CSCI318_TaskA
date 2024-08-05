30bc3d3c91a7e25008de9f5212a6159fe100dd34
LeeWonBeen /  이원빈 <devacclee000907@gmail.com>
2024-08-05 03:30:26 +0000

    added proper .devcontainer , checked running application

---
+++ src/main/java/JOME/TaskA/controller/HelloController.java
@@ -0,0 +1,15 @@
+package JOME.TaskA.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController { 
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }   
+
+}
